968f78326779621352eef9d0ddd74939171d9b7b
sara <taheris774@gmail.com>
2025-07-24 02:42:18 -0400

    add test example

--- steering.py
+++ steering.py
@@ -2,3 +2,6 @@ def calculate_steering_angle (x_offset, y_offset)
 	import math
 	angle = math.degrees(math.atan2(x_offset, y_offset))
 	return angle
+if __name__ == "__main__":
+	angle = calculate_steering_angle(10, 5)
+	print("Steering angle:", angle)
